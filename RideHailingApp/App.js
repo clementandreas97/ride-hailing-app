c389fadd020ca548888ce3e29bebd2386e575730
Christopher Clement Andreas <clement.andreas97@gmail.com>
2019-11-24 22:20:38 +0700

    Add Search Bar Section and draft Header

--- RideHailingApp/App.js
+++ RideHailingApp/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={{flex: 1}}>
-      <View style={{flex: 1, backgroundColor: 'pink'}}></View>
+      <View style={{flex: 1, backgroundColor: 'white'}}>
+        {/* SearchBar Section */}
+        <View style={{paddingHorizontal: 16, flexDirection: 'row', marginTop: 68, alignItems: 'center'}}>
+          <View style={{flex: 1, position: 'relative'}}>
+            <View style={{backgroundColor: 'pink', width: 16, height: 16, position: 'absolute', top: 12, left: 12}}></View>
+            <TextInput placeholder='Current Text Value' style={{paddingLeft: 32, borderWidth: 1, borderColor: '#E8E8E8', borderRadius: 25, height: 40, fontSize: 12}}></TextInput>
+          </View>
+          <View style={{marginLeft: 16}}>
+            <View style={{backgroundColor: 'pink', width: 36, height: 36}}></View>
+          </View>
+        </View>
+        {/* Header Section */}
+        <View>
+
+        </View>
+      </View>
       <View style={styles.tabBar}> 
         <View style={[styles.tabBarContainer]}>
           <View style={styles.tabBarIcon}></View>
@@ -42,7 +57,9 @@ const styles = StyleSheet.create({
     height: 68,
     backgroundColor: 'white',
     flexDirection: 'row',
-    justifyContent: 'space-around'
+    justifyContent: 'space-around',
+    borderTopWidth: 1,
+    borderTopColor: '#E8E8E8'
   },
   tabBarContainer: {
     flex: 1, 
