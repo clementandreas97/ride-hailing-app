3b52f028f5feaca33ea7d8a7382615cc7b447e3a
Christopher Clement Andreas <clement.andreas97@gmail.com>
2019-11-24 21:58:12 +0700

    Add bottom tab bar without icon

--- RideHailingApp/App.js
+++ RideHailingApp/App.js
@@ -3,8 +3,30 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{flex: 1}}>
+      <View style={{flex: 1, backgroundColor: 'pink'}}></View>
+      <View style={styles.tabBar}> 
+        <View style={[styles.tabBarContainer]}>
+          <View style={styles.tabBarIcon}></View>
+          <Text style={styles.tabBarTextStyle}>Home</Text>
+        </View>
+        <View style={[styles.tabBarContainer]}>
+          <View style={styles.tabBarIcon}></View>
+          <Text style={styles.tabBarTextStyle}>Orders</Text>
+        </View>
+        <View style={[styles.tabBarContainer]}>
+          <View style={styles.tabBarIcon}></View>
+          <Text style={styles.tabBarTextStyle}>Help</Text>
+        </View>
+        <View style={[styles.tabBarContainer]}>
+          <View style={styles.tabBarIcon}></View>
+          <Text style={styles.tabBarTextStyle}>Inbox</Text>
+        </View>
+        <View style={[styles.tabBarContainer]}>
+          <View style={styles.tabBarIcon}></View>
+          <Text style={styles.tabBarTextStyle}>Account</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -12,8 +34,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'pink',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  tabBar: {
+    height: 68,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  tabBarContainer: {
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'flex-start',
+    paddingTop: 8
+  },
+  tabBarTextStyle: {
+    fontSize: 10,
+    color: '#545454'
+  },
+  tabBarIcon: {
+    width: 26,
+    height: 26,
+    backgroundColor: 'white'
+  }
 });
